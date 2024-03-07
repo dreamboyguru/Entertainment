2aee09512bf276060980534c0fbc2a26425f041d
Gurusidda Hanamannavr <143526836+dreamboyguru@users.noreply.github.com>
2024-03-07 13:49:44 +0530

    Update Tv.js
    add invoke

--- src/camponants/TV/Tv.js
+++ src/camponants/TV/Tv.js
@@ -21,7 +21,7 @@ const Tv = ( type ) => {
             // console.log('token is empty...! Please Login First');
             setshowModel(true)
         } else {        
-            axios.post('https://entertainmentbackendott.onrender.com/bookmark', { email: userName, video_id: value, type: 'tv' })
+            axios.post(`${process.env.REACT_APP_INVOKE}/bookmark`, { email: userName, video_id: value, type: 'tv' })
             .then(response => {
 
                 console.log('Response from server:', response.data.video_id);
@@ -41,7 +41,7 @@ const Tv = ( type ) => {
         if (!token && !userName) {
             console.log('token is empty...! Please Login First');
         } else {        
-            axios.delete(`https://entertainmentbackendott.onrender.com/bookmark/${value}`)
+            axios.delete(`${process.env.REACT_APP_INVOKE}/bookmark/${value}`)
             .then(response => {
                 window.location.reload()
                 console.log(response);
@@ -67,7 +67,7 @@ const Tv = ( type ) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`https://entertainmentbackendott.onrender.com/tv/${userName}`);
+                const response = await axios.get(`${process.env.REACT_APP_INVOKE}/tv/${userName}`);
                 // console.log(response.data);
                 dispatch(gettv(response.data));
             } catch (err) {
